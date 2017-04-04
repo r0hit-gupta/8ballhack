3150ed086299d7bf3d249e42b8c35c37737033f7
r0hit-gupta <rohit1997gupta@gmail.com>
2017-04-05 01:31:43 +0530

    comments

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,9 +1,9 @@
 // p5 can not be executed the normal "global" way
 // Instead a sketch instance has to be manually created
 // This is done with the closure below
-var sketch = function(p5) {
 
-  // The setup function
+var sketch = function(p5) {
+  // setup canvas
   p5.setup = function() {
     console.log('p5 running');
     var c = p5.createCanvas(p5.windowWidth, p5.windowHeight);
@@ -14,8 +14,7 @@ var sketch = function(p5) {
     p5.noLoop();
     p5.stroke(255);
   }
-
-  // Draw p5.lines if the user drags the mouse
+  // draw the guidelines
   p5.draw = function() {
 		p5.clear();
 		p5.line(0,0, p5.mouseX, p5.mouseY);
@@ -26,7 +25,7 @@ var sketch = function(p5) {
 		p5.line(570,274, p5.mouseX, p5.mouseY);
 		p5.ellipse(p5.mouseX, p5.mouseY, 16);
 	}
-
+  // check if SHIFT key is pressed to change the layout
   p5.mouseMoved = function() {
     if(p5.keyIsDown(p5.SHIFT)){
       p5.draw();
